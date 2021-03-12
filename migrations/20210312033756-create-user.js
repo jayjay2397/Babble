14fbe186acfdde50c24050ecbaf1ad257497a9fa
Jay Valdez <jj.valdez09@gmail.com>
2021-03-11 22:38:19 -0600

    adding the apollo server and making the connection for mysql

---
+++ migrations/20210312033756-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unqiue: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unqiue: truesequ
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('u desers');
+  }
+};
