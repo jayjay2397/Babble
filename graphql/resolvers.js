5e0252e1df08d9e0b2d4de3e49b3c67d70d5576a
DianaTrujillo-create <diana.trujillo0806@gmail.com>
2021-03-17 23:47:54 -0500

    commit to main

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,6 @@
-const user = require("../models/user")
+const bcrypt = require('bcryptjs')
+const { UserInputError } = require('apollo-server')
+const { User } = require("../models/user")
 
 module.exports = {
     Query: {
@@ -14,12 +16,35 @@ module.exports = {
     },
     Mutation: {
         register: async (_, args) => {
-            const { username, email, password, confirmPassword } = args
+            let { username, email, password, confirmPassword } = args
 
             try {
              // TODO: Validate data that has been input 
+             if(email.trim() === '') 
+                errors.email = 'email must not be empty'
+             if(username.trim() === '') 
+                errors.username = 'username must not be empty'
+             if(password.trim() === '') 
+                errors.password = 'password must not be empty'
+             if(confirmPassword.trim() === '') 
+                errors.confirmPassword = 'repeat password must not be empty'
 
-             // TODO: Check if username or email alreadt exist
+             if(password !== confirmPassword) errors.confirmPassword = 'password must match'
+
+             // TODO: Check if username or email already exist
+             const userByUsername = await User.findOne({ where: { username }})
+             const userByEmail = await User.findOne({ where: { email }})
+
+             if(userByUsername) errors.username = 'Username is taken'
+             if(userByEmail) errors.email = 'Email is taken'
+
+             if(Object.keys(errors).lenght > 0){
+                 throw errors
+             }
+
+
+             // TODO: Hash Password
+             password = await bcrypt.hash(password, 6)
 
              // TODO: Create user
              const user = await User.create({
@@ -32,7 +57,7 @@ module.exports = {
              return user 
             } catch(err){   
               console.log(err)
-              throw err  
+              throw new UserInputError('Bad input', err) 
             }
         }
     }
