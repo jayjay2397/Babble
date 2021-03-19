8b6d15bb70699fcef82927bce932f845ac3c0a19
Jay Valdez <jj.valdez09@gmail.com>
2021-03-19 00:05:31 -0500

    adding the registration form with scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import './App.css';
+import {Card,Container , Row , Col, Form , Button } from 'react-bootstrap'
+import BG1 from './video/bg1.mp4';
+
+
+import './App.scss';
 
 function App() {
   return (
-    <div> 
-      <h1> Hellooooo babblers </h1>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1> Register</h1>
+          <Form>
+            <Form.Group>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+            <div className = "text-center">
+            <Button variant="success" type="submit">
+              Submit
+            </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+      <video 
+             id ="VidBG" autoPlay muted loop> <source src={BG1} type="video/mp4"/>
+            </video>
+    </Container>
   );
 }
 
