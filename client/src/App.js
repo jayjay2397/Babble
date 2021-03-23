f13a3bdfb2d259ed2d1a26e0c126a0617804f321
Jay Valdez <jj.valdez09@gmail.com>
2021-03-23 18:06:13 -0500

    rearranged some files into a client folder

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Container } from 'react-bootstrap'
+
+import ApolloProvider from './ApolloProvider'
+
+
+import './App.scss';
+import Register from './pages/Register'
+
+
+
+function App() {
+  return (
+    <ApolloProvider>
+    <Container className= "pt-5">
+      <Register/>
+    </Container>
+    </ApolloProvider>
+  );
+}
+
+export default App;
