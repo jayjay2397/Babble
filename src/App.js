8e9fc30f000cca73c5b164fbada6b624276ea5c2
Jay Valdez <jj.valdez09@gmail.com>
2021-03-23 20:23:26 -0500

    adding the home and login pages and routing them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Container } from 'react-bootstrap'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import ApolloProvider from './ApolloProvider'
 
 
 import './App.scss';
+import Home from './pages/Home';
+import Login from './pages/Login';
 import Register from './pages/Register'
 
 
@@ -12,9 +15,13 @@ import Register from './pages/Register'
 function App() {
   return (
     <ApolloProvider>
+      <BrowserRouter>
     <Container className= "pt-5">
-      <Register/>
+      <Route path="/register" component={Register}/>
+      <Route exact path="/" component={Home}/>
+      <Route path="/login" component={Login}/>
     </Container>
+    </BrowserRouter>
     </ApolloProvider>
   );
 }
